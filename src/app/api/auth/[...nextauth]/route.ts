2550b5668388714113cd10dbb6d4ffc7b4c84999
Kartikey1908 <ykartikey1908@gmail.com>
2024-08-30 02:04:36 +0530

    change to increase duration2

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import { authOptions } from "./options";
 
-export const maxDuration = 30;
+export const maxDuration = 300;
 
 const handler = NextAuth(authOptions);
 
