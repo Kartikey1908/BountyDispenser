35b77a98d1bfdc1c83bdec8f01353a538740088c
Kartikey1908 <ykartikey1908@gmail.com>
2024-08-31 23:02:46 +0530

    made bounties page dynamic3

--- src/app/api/get-bounties/route.ts
+++ src/app/api/get-bounties/route.ts
@@ -4,7 +4,7 @@ import Bounty from '@/model/Bounty';
 import { NextRequest, NextResponse } from 'next/server';
 
 // export const dynamic = 'force-dynamic';
-export const revalidate = 1;
+export const revalidate = 0;
 
 
 export async function GET(req : NextRequest, res : NextResponse) {
